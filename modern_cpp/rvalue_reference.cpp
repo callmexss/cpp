33b5b4de6931cf81b9c9c5a365429887a0d4e2fa
callmexss <callmexss@126.com>
2019-07-09 19:50:40 +0800

    C++ 11 Rvalue Reference 3 Perfect Forwarding

--- modern_cpp/rvalue_reference.cpp
+++ modern_cpp/rvalue_reference.cpp
@@ -10,8 +10,13 @@
 #include <iostream>
 #include <cassert>
 #include <vector>
+#include <typeinfo>
 
 using namespace std;
+
+/**
+ * Usage 1: Moving Semantics
+ */
 
 // lvalue reference and rvalue reference can be used to overload function
 void printInt(int &i) { cout << "&i: " << i << endl; }
@@ -122,8 +127,128 @@ Dog &createDogByRef()
     return d;
 }
 
+/**
+ * Note 1：the most useful place for rvalue reference is ouerloading copy 
+ * constructor and copy assignment operator, to achieve move semantics.
+ */
+class X
+{
+public:
+    X &operator=(X const &rhs);
+    X &operator=(X &&rhs);
+};
+
+X &X::operator=(X const &rhs) {}
+X &X::operator=(X &&rhs) {}
+
+/**
+ *  Note 2: Move semantics is implemented for all STL containers, which means:
+ *      a. Move to C++ 11, Your code will be faster without changing a thing.
+ *      b. Passing-By-Value can be used for STL containers.
+ */
+
+vector<int> foo()
+{
+    vector<int> myvector;
+    //…
+    return myvector;
+}
+
+void hoo(string s);
+
+bool goo(vector<int> &arg); //Pass by reference if you use arg to carry
+                            //data back from goo（）
+
+/* Move Constructor / Move Assignment Operator:
+ * Purpose: conveniently auoid costly and unnecessary deep copying.
+ * 
+ * 1. They are particularly powerful where passing by reference and
+ *    passing by ualue are both used.
+ * 
+ * 2. They give you finer control of which part of your object to
+ *    be moved.
+ */
+
+/**
+ * Usage 2: Perfect Forwarding
+ */
+
+// XuVector has both move constructor and copy constructor
+void foo_new(XuVector arg) { cout << &arg << endl; }
+
+template <typename T>
+void relay1(T arg)
+{
+    foo_new(arg);
+}
+
+/*
+ * 1. No costly and unnecessary copy construction of boVector is made;
+ * 2. rvalue is forwarded as rvalue, and lvalue is forwarded as lvalue.
+ */
+
+// Solution
+template <typename T>
+void relay2(T &&arg)
+{
+    foo_new(std::forward<T>(arg));
+}
+
+// Note: this will work because type T is a template type.
+
+/*
+ * Reference Collapsing Rules (C++11):
+ * 1.   T& &   ==>  T&
+ * 2.   T& &&  ==>  T&
+ * 3.   T&& &  ==>  T&
+ * 4.   T&& && ==>  T&&
+ */
+
+// template <class T>
+// struct remove_reference; // It removes reference on type T
+
+// T is int&
+remove_reference<int &>::type i; // int i;
+
+// T is int
+remove_reference<int>::type i1; // int i;
+
+template <typename T>
+void relay3(T &&arg)
+{
+    cout << arg << endl;
+    // cout << typeid(arg).name << endl;
+}
+
+// T&& is called universal reference
+
+// T&& is Universal Reference: rvalue, lvalue, const, non-const, etc...
+// Conditions:
+// 1. T is a template type.
+// 2. Type deduction (reference collasing) happens to T.
+//    -T is a function template type, not class template type.
+
+//Implementation of std::forward()
+// template <class T>
+// T &&forward(typename remove_reference<T>::type &arg)
+// {
+//     return static_cast<T &&>(arg);
+// }
+
+template <typename T>
+void relay4(T &&arg)
+{
+    foo_new(forward<T>(arg));
+}
+
+// std::move()vs std::forward()
+// std::move<T>(arg); // Turn arg into an rvalue type 
+// std::forward<T>(arg); // Turn arg to type of T&&
+
+
 int main(int argc, char const *argv[])
 {
+    cout << "----------Move Semantics----------" << endl;
     // What is rvalue reference
     int a = 5;   // a is a lvalue
     int &b = a;  // b is a lvalue reference
@@ -157,45 +282,29 @@ int main(int argc, char const *argv[])
     // reusable is distroyed here
 
     foo(createXuVector(n), true);
+    cout << "-------End of Move Semantics------" << endl;
+
+    cout << "--------Perfect Forwarding--------" << endl;
+    cout << "XuVector reusable_new = createXuVector(n);" << endl;
+    XuVector reusable_new = createXuVector(n);
+    cout << "relay(reusable_new);" << endl;
+    relay1(reusable_new);
+    cout << "relay(createXuVector(n));" << endl;
+    relay2(createXuVector(n));
+
+    /*
+    * rvalue reference is specified with type&.
+    *
+    * type&& is rvalue reference?
+    */
+
+    // T&& variable is intialized with rvalue => rvalue reference
+    relay3(9); // T = int && -> T && = int && && = int &&
+
+    // T&& variable is intialized with lvalue =>lvalue reference
+    relay3(n); // T = int&   -> T && = int& &&   = int&
+
+    cout << "------End Perfect Forwarding------" << endl;
 
     return 0;
 }
-
-/**
- * Note 1：the most useful place for rvalue reference is ouerloading copy 
- * constructor and copy assignment operator, to achieve move semantics.
- */
-class X
-{
-public:
-    X &X::operator=(X const &rhs);
-    X &X::operator=(X &&rhs);
-};
-
-/**
- *  Note 2: Move semantics is implemented for all STL containers, which means:
- *      a. Move to C++ 11, Your code will be faster without changing a thing.
- *      b. Passing-By-Value can be used for STL containers.
- */
-
-vector<int> foo()
-{
-    vector<int> myvector;
-    //…
-    return myvector;
-}
-
-void hoo(string s);
-
-bool goo(vector<int> &arg); //Pass by reference if you use arg to carry
-                            //data back from goo（）
-
-/* Move Constructor / Move Assignment Operator:
- * Purpose: conveniently auoid costly and unnecessary deep copying.
- * 
- * 1. They are particularly powerful where passing by reference and
- *    passing by ualue are both used.
- * 
- * 2. They give you finer control of which part of your object to
- *    be moved.
- */
